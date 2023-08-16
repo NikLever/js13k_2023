151842ee7925a89d4dd2a12a7892c9082fe8e37f
NikLever <nik.j.lever@gmail.com>
2023-08-16 08:14:44 +0100

    Knight now has animations

--- src/Knight.js
+++ src/Knight.js
@@ -1,17 +1,17 @@
 class Knight{
-    constructor(scene){
-        this.root = this.createModel();
+    constructor(scene, black=false){
+        this.root = this.createModel(black);
         const light = new THREE.PointLight(0xFFFFFF, 2, 5)
         light.position.set(0, 3, 1);
         this.root.add(light);
         scene.add(this.root);
     }
 
-    createModel(){
+    createModel(black){
         const gSkirt = new THREE.CylinderGeometry(0.4, 0.6, 0.5, 32, 1, true );
         const gHead = new THREE.SphereGeometry(0.4, 24, 10);
         const pHelmet = [
-            new THREE.Vector2(0.5, 0),
+            new THREE.Vector2(0.5, (black) ? -0.32: 0),
             new THREE.Vector2(0.5, 0.2),
             new THREE.Vector2(0.45, 0.2),
             new THREE.Vector2(0.4, 0.3),
@@ -30,11 +30,13 @@ class Knight{
         const gTunic = new THREE.LatheGeometry(pTunic, 12);
         const gBelt = new THREE.CylinderGeometry(0.45, 0.45, 0.2, 32, 1, false);
 
-        const mSkirt = new THREE.MeshStandardMaterial( { color: 15991041 } );
-        const mHead = new THREE.MeshStandardMaterial( { color: 16373422 } );
-        const mHelmet = new THREE.MeshStandardMaterial( { color: 0xC7C7C7 } );
-        const mTunic = new THREE.MeshStandardMaterial( { color: 16777215 } );
-        const mBelt = new THREE.MeshStandardMaterial( { color: 12615993 } );
+        const colours = (black) ? [0x0A0A0A, 0xC8AD8D, 0x3B3845, 0x423D4D, 0x000000 ] : [ 15991041, 16373422, 0xC7C7C7, 16777215, 12615993 ]
+
+        const mSkirt = new THREE.MeshStandardMaterial( { color: colours[0] } );
+        const mHead = new THREE.MeshStandardMaterial( { color: colours[1] } );
+        const mHelmet = new THREE.MeshStandardMaterial( { color: colours[2] } );
+        const mTunic = new THREE.MeshStandardMaterial( { color: colours[3] } );
+        const mBelt = new THREE.MeshStandardMaterial( { color: colours[4] } );
 
         const root = new THREE.Group();
         const skirt = new THREE.Mesh( gSkirt, mSkirt );
@@ -58,7 +60,7 @@ class Knight{
             0.7856016096417388,0.15829722622603848,1]);
         root.add(holster);
 
-        this.sword = this.createSword();
+        this.sword = this.createSword(black);
         holster.add( this.sword );
 
         root.traverse( object => {
@@ -67,19 +69,29 @@ class Knight{
             }
         });
 
-        this.animClips = {};
+        this.animactions = {};
+
         const config1 = {
-            duration: 0.3,
+            duration: 0.4,
             times: [0, 0.1, 0.3],
             pos:[{ x:0, y:0, z:0 }, { x:-0.261, y:0.522, z:0.201 }, { x:-0.293, y:0.722, z:0.861 }],
             rot:[{ x:0, y:0, z:0 }, { x:21.69, y:13.79, z:-9.18 }, { x:-2.23, y:4.21, z:175.94 }]
         }
-        this.animClips.drawsword = this.createAnim('drawsword', config1);
+        const drawsword = this.createAnim('drawsword', config1);
+
+        const config2 = {
+            duration: 0.45,
+            times: [0, 0.05, 0.1, 0.25, 0.3, 0.45],
+            pos:[{ x:-0.293, y:0.722, z:0.861 }, { x:-0.928, y:0.556, z:-0.094 }, { x:-0.529, y:0.070, z:-0.026 }, { x:-0.848, y:0.733, z:-0.150 }, { x:-1.038, y:0.755, z:0.720 }, { x:-0.293, y:0.722, z:0.861 }],
+            rot:[{ x:-2.23, y:4.21, z:175.94 }, { x:-111.33, y:18.07, z:-107.23 }, { x:29.51, y:-2.52, z:-41.79 }, { x:-86.10, y:6.01, z:-115.58 }, { x:-65.92, y:13.74, z:-39.0 }, { x:-2.23, y:4.21, z:175.94 }]
+        }
+        const usesword = this.createAnim('usesword', config2);
 
         this.mixer = new THREE.AnimationMixer(this.sword);
-        this.action = this.mixer.clipAction(this.animClips.drawsword);
-        this.action.clampWhenFinished = true;
-        this.action.loop = THREE.LoopOnce;
+        this.animactions.drawaction = this.mixer.clipAction(drawsword);
+        this.animactions.drawaction.clampWhenFinished = true;
+        this.animactions.drawaction.loop = THREE.LoopOnce;
+        this.animactions.useaction = this.mixer.clipAction(usesword);
 
         return root;
     }
@@ -103,7 +115,7 @@ class Knight{
 		return new THREE.AnimationClip( name, config.duration, [ pos, rot ] );
     }
 
-    createSword(){
+    createSword(black){
         const gBlade = new THREE.BoxGeometry( 0.22, 0.5, 0.05, 2, 1, 1 );
         const pBlade = gBlade.getAttribute('position');
         const v = new THREE.Vector3();
@@ -118,9 +130,10 @@ class Knight{
         const gCrossBar = new THREE.BoxGeometry(0.28, 0.04, 0.18);
         const gCrossBarEnd = new THREE.CapsuleGeometry(0.1, 0.3, 4, 12);
         
-        const mBlade = new THREE.MeshStandardMaterial( { color: 16777215, emissive: 6381921 } );
-        const mHandle = new THREE.MeshStandardMaterial( { color: 6458346 } );
-        const mGold = new THREE.MeshStandardMaterial( { color: 16774938, emissive: 6381921 } );
+        const colours = (black) ? [0x999999, 0x50545E, 0x706E3E] : [16777215, 6458346, 16774938 ]
+        const mBlade = new THREE.MeshStandardMaterial( { color: colours[0], emissive: 6381921 } );
+        const mHandle = new THREE.MeshStandardMaterial( { color: colours[1] } );
+        const mGold = new THREE.MeshStandardMaterial( { color: colours[2], emissive: 6381921 } );
 
         const root = new THREE.Group();
         const blade = new THREE.Mesh( gBlade, mBlade );
@@ -143,6 +156,37 @@ class Knight{
         crossBar.add(crossBarEnd2);
 
         return root;
+    }
+
+    setDirection( v ){
+        if (!v || v.length()==0) return;
+        const direction = v.clone().multiplyScalar(10);
+        direction.y = 0;
+        const target = this.root.position.clone().add(direction);
+        const q = this.root.quaternion.clone();
+        this.root.lookAt(target);
+        this.root.quaternion.slerp(q, 0.9);
+    }
+
+    playAnim(name){
+        console.log(`playAnim(${name})`);
+
+        this.stopAnims();
+
+        if (name=='drawaction'){
+            this.mixer.addEventListener('finished', this.playAnim.bind(this, 'useaction'));
+        }else{
+            this.mixer.removeEventListener('finished');
+        }
+
+        this.animactions[name].play();
+    }
+
+    stopAnims(){
+        for( const [key, action] of Object.entries(this.animactions) ) {
+            action.stop();
+            action.reset();
+        };     
     }
 
     update(dt){
